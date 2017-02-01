4a49aee733579e3e7bd3b105410a90ea67d7c800
rick baker <baker.rickw@gmail.com>
2017-01-31 20:32:38 -0700

    updates

--- main.js
+++ main.js
@@ -4,10 +4,17 @@ var fs = require('fs');
 var port = 8080;
 
 http.createServer( function(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/html'});
+     //change the Content type for text or html
     //response.end("My first node app");
     fs.readFile('index.html', function(err, content) {
-        response.end(content);
+        if (err) {
+            response.writeHead(404, {'Content-Type': 'text/html'});
+            response.end("failed");
+        } else {
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.end(content);
+        }
+        
     });
     
 }).listen(port); //http create server will listen on on this port
