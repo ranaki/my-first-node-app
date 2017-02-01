88fdcc27968fbee6d739aee99d9879b131f289ff
rick baker <baker.rickw@gmail.com>
2017-01-31 20:12:01 -0700

    updating node

--- main.js
+++ main.js
@@ -1,10 +1,15 @@
 var http = require('http');
 var fs = require('fs');
+
 var port = 8080;
 
 http.createServer( function(request, response) {
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    response.end("My first node app");
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    //response.end("My first node app");
+    fs.readFile('index.html', function(err, content) {
+        response.end(content);
+    });
+    
 }).listen(port); //http create server will listen on on this port
 
 console.log('server running on port ' + port);
